77c7df2df1b1d52116b11b932df6f827c2a5c530
Code Nation <codenation@Codes-MacBook-Pro.local>
2020-02-17 14:20:00 +0000

    Organise

--- src/App.js
+++ src/App.js
@@ -10,15 +10,7 @@ class App extends Component {
     currentItem: {text:"", key:""}
   }
 
-  handleInput = e => {
-    const itemText = e.target.value
-    const currentItem = { text: itemText, key: Date.now() }
-    this.setState({
-      currentItem,
-    })
-  }
-
-  addItem = e => {
+  addItem = (e) => {
     e.preventDefault()
     const newItem = this.state.currentItem
     
@@ -31,7 +23,15 @@ class App extends Component {
     }
   }
 
-  deleteItem = key => {
+  handleInput = (e) => {
+    const itemText = e.target.value
+    const currentItem = { text: itemText, key: Date.now() }
+    this.setState({
+      currentItem
+    })
+  }
+
+  deleteItem = (key) => {
     const filteredItems = this.state.items.filter(item => {
       return item.key !== key
     })
@@ -46,8 +46,8 @@ class App extends Component {
         <TodoList
           addItem={this.addItem}
           inputElement={this.inputElement}
-          handleInput={this.handleInput}
           currentItem={this.state.currentItem}
+          handleInput={this.handleInput}
         />
         <TodoItems entries={this.state.items} deleteItem={this.deleteItem} />
       </div>
