314d7af075e439862b3a3e71496f720441d1e624
Daniel Sutcliffe <dannysutcliffe@hotmail.com>
2020-02-28 09:57:15 +0000

    Update

--- src/components/TodoItems.js
+++ src/components/TodoItems.js
@@ -1,20 +1,50 @@
+// import React, { Component } from 'react'
+
+// class TodoItems extends Component {
+
+//   createTasks = (item) => {
+//     return (
+//       <li key={item.key} onClick={() => this.props.deleteItem(item.key)}>
+//         {item.text}
+//       </li>
+//     )
+//   }
+  
+//   render() {
+//     const todoEntries = this.props.entries
+//     const listItems = todoEntries.map(this.createTasks)
+
+//     return <ul className="theList">{listItems}</ul>
+//   }
+// }
+
+// export default TodoItems
+
 import React, { Component } from 'react'
 
 class TodoItems extends Component {
 
   createTasks = (item) => {
-    return (
-      <li key={item.key} onClick={() => this.props.deleteItem(item.key)}>
-        {item.text}
-      </li>
+
+    const todoList = item.length ? (
+      item.map(item => {
+        return (
+          <div className="collection-item" key={item.id}>
+          <span>{item.content}</span>
+          </div>
+        )
+      })
+    ) : (
+      <p className="center">You jave no todos left</p>
     )
   }
-  
-  render() {
-    const todoEntries = this.props.entries
-    const listItems = todoEntries.map(this.createTasks)
 
-    return <ul className="theList">{listItems}</ul>
+  render() {
+    return (
+      <div className="todos collection">
+        {this.createTasks}
+      </div>
+    )
   }
 }
 
